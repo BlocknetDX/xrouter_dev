976fca2e09eafd2470dbd6ff02392856c523cd1b
beardhero <damienburbine@gmail.com>
2020-07-15 14:11:49 -0600

    dockerfile, listenjs fixes

--- oracle/js/listener.js
+++ oracle/js/listener.js
@@ -1,3 +1,15 @@
+const Web3 = require('web3')
+
+const web3 = new Web3(new Web3.providers.WebsocketProvider('http://127.0.0.1:8545'))
+
+console.log(web3.version)
+
+web3.eth.isSyncing().then(console.log)
+
+const abi = require('./solrouter.json')
+//GNT4 on ropsten
+const contract = new web3.eth.Contract(abi, '0x219DaB9993AAcf345B25D89AA5ACa639CC70e65d')
+
 let lastBlock = 0
 let newQueries = []
 setInterval(async function() {
@@ -10,8 +22,8 @@ setInterval(async function() {
       if(events){
         console.log(events);
         if(events.length > 0){
-            lastBlock = events[events.length - 1].blocknumber
-            foreach(let e of events){
+            lastBlock = events[events.length - 1].blocknumber;
+            for(let e of events){
                 if(e.event == "queried"){
                     newQueries.push(e)
                 }
